f7617ae4aa64812317c0a146cacbc0b28e67ed22
Cristian Alvarez Hernandez <alvarezhernandezcristian1@gmail.com>
2018-04-05 11:36:05 -0500

    Implemento la notificacion para parar el sonido

--- app/src/main/java/miercoles/dsl/resproductorsonidos/SonidosService.java
+++ app/src/main/java/miercoles/dsl/resproductorsonidos/SonidosService.java
@@ -1,15 +1,22 @@
 package miercoles.dsl.resproductorsonidos;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 public class SonidosService extends Service {
     public static final String PARAR_SONIDO = "parar";
     public static final String INICIAR_SONIDO = "iniciar";
+    public static final String PARAR_SONIDO_Y_MANDAR_A_MAIN_ACTIVITY = "parar_mandar";
 
     private MediaPlayer mediaPlayer;
+    private NotificationCompat.Builder builder;
 
     public SonidosService() {
     }
@@ -18,6 +25,19 @@ public class SonidosService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        builder = new NotificationCompat.Builder(this, "michannnel")
+                .setSmallIcon(android.R.drawable.stat_notify_chat)
+                .setContentTitle("Sonando")
+                .setContentText("Click para detener");
+
+        Intent intentPararSonido = new Intent(this, SonidosService.class);
+        intentPararSonido.setAction(PARAR_SONIDO_Y_MANDAR_A_MAIN_ACTIVITY);
+        PendingIntent pendingIntent = PendingIntent.getService(this, 0, intentPararSonido, 0);
+
+        builder.setContentIntent(pendingIntent);
+
+
 
         mediaPlayer = MediaPlayer.create(this, R.raw.halloween);
         mediaPlayer.setLooping(true);// Se repite el audio ciclicamente
@@ -35,10 +55,20 @@ public class SonidosService extends Service {
                     mediaPlayer.release();
 
                     stopSelf();
+                }else if(accion.equals(PARAR_SONIDO_Y_MANDAR_A_MAIN_ACTIVITY)){
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+
+                    stopSelf();
+
+                    Intent intentMainActivity = new Intent(this, MainActivity.class);
+                    intentMainActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intentMainActivity);
                 }
             } else {
                 if (accion.equals(INICIAR_SONIDO)) {
                     mediaPlayer.start();
+                    startForeground(488, builder.build());
                 }
             }
 
